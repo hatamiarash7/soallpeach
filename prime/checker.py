5d80345e16b39b4f3fa1e0ad73660ccd8a0fcea0
Arash Hatami <hatamiarash7@gmail.com>
2020-04-13 12:56:59 +0430

    get file from args

--- prime/checker.py
+++ prime/checker.py
@@ -1,5 +1,7 @@
 import math, sys
 
+input_file = sys.argv[1]
+
 def is_prime(n: int) -> bool:
     for x in range(2, int(math.sqrt(n))):
         if n % x == 0:
@@ -7,6 +9,6 @@ def is_prime(n: int) -> bool:
     else:
         return True
 
-with open('input.txt', 'r') as numbers:
+with open(input_file, 'r') as numbers:
     for number in numbers:
         print(1 if is_prime(int(number)) else 0)
